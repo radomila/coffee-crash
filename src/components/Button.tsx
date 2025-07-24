26453ced907df5314d094e6f05b57fc552427e0c
Radomila Hejhalová <hejhalova.radomila@gmail.com>
2025-07-24 09:06:22 +0200

    Added buttons styling when active

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,21 @@
+import type { ActiveButtonType } from '../types.ts';
+import { useActiveButton } from '../hooks/useActiveButton.ts';
+import clsx from 'clsx';
+
 interface Props {
   title: string;
+  value?: ActiveButtonType;
   onClick?: () => void;
 }
 
-export function Button({ title, onClick }: Props) {
+export function Button({ title, value, onClick }: Props) {
+  const { activeButton } = useActiveButton();
+
+  const isActive = activeButton === value;
+
   return (
     <button
-      className="text-text-primary bg-button text-label px-3 py-1.5 rounded-lg font-semibold cursor-pointer"
+      className={clsx('text-text-primary text-label px-3 py-1.5 rounded-lg font-semibold cursor-pointer', isActive ? 'bg-button' : 'bg-transparent')}
       onClick={onClick}
     >
       {title}
--- src/components/ButtonList.tsx
+++ src/components/ButtonList.tsx
@@ -3,14 +3,17 @@ import { useActiveButton } from '../hooks/useActiveButton.ts';
 
 export function ButtonList() {
   const { setActiveButton } = useActiveButton();
+
   return (
     <div className="flex flex-row flex-wrap gap-3">
       <Button
         title="All Products"
+        value="all"
         onClick={() => setActiveButton('all')}
       />
       <Button
         title="Available Now"
+        value="available"
         onClick={() => setActiveButton('available')}
       />
     </div>
